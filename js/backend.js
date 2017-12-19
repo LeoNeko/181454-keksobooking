ca25c2e1d7323094a649e3e646b3aebe1eb6fff6
Maksim <maxsimshiba@yandex.ru>
2017-12-19 19:06:41 +0300

    Реализован функционал отправки с формы и получение данных с сервера

---
+++ js/backend.js
@@ -0,0 +1,85 @@
+'use strict';
+
+(function () {
+
+  var setup = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    return xhr;
+  };
+
+  var backend = {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          var userDialog = document.querySelector('.map');
+          var similarListElement = userDialog.querySelector('.map__pins');
+          var fragment = document.createDocumentFragment();
+
+          // Цикл формирования разметки плашек
+          for (var i = 0; i < xhr.response.length; i++) {
+            fragment.appendChild(window.renderRented(xhr.response[i], xhr.response));
+          }
+
+          // отрисовка плашек похожих объявлений
+          similarListElement.appendChild(fragment);
+        } else {
+          onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000; // 10s
+
+      xhr.open('GET', 'https://1510.dump.academy/keksobooking/data');
+      xhr.send();
+    },
+    save: function (data, onSuccess, onError) {
+      var xhr = setup(onSuccess, onError);
+
+      xhr.open('POST', 'https://1510.dump.academy/keksobooking');
+      xhr.send(data);
+    },
+    errorHandler: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '30px';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    }
+  };
+
+  window.backend = backend;
+})();
--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Число сдаваемых аппартаментов
-var rentaCount = 8;
+// var rentaCount = 8;
 // Пустой массив для заполнения сдаваемых аппартаментов
-var rentaArr = [];
+// var rentaArr = [];
 
 // Показывает скрытый блок карты
 var userDialog = document.querySelector('.map');
@@ -11,9 +11,9 @@ var userDialog = document.querySelector('.map');
 var pinStart = document.querySelector('.map__pin--main');
 var formElement = document.querySelector('.notice__form');
 var formFieldsets = document.getElementsByClassName('form__element');
-var similarListElement = userDialog.querySelector('.map__pins');
-var fragment = document.createDocumentFragment();
-
+// var similarListElement = userDialog.querySelector('.map__pins');
+// var fragment = document.createDocumentFragment();
+var form = document.querySelector('.notice__form');
 // window.fillRented(RentaCount);
 
 window.fieldsetsToggle(formFieldsets);
@@ -25,19 +25,25 @@ window.fieldsetsToggle(formFieldsets);
 */
 
 //
-rentaArr = window.fillRented(rentaCount);
+// rentaArr = window.fillRented(rentaCount);
+var onError = function (message) {
+  console.error(message);
+};
+
+var onSuccess = function (data) {
+  console.log(data);
+};
 
 function activeSitePageHandle() {
   userDialog.classList.remove('map--faded'); // Убрать затемнение с карты
   formElement.classList.remove('notice__form--disabled'); // Убрать затемнение с карты формы
   window.fieldsetsToggle(formFieldsets);
 
-  // Цикл формирования разметки плашек
-  for (var i = 0; i < rentaArr.length; i++) {
-    fragment.appendChild(window.renderRented(rentaArr[i], rentaArr));
-  }
   // отрисовка плашек похожих объявлений
-  similarListElement.appendChild(fragment);
+  // similarListElement.appendChild(fragment);
+
+  // Загрузка данных с сервера
+  window.backend.load(onSuccess, onError);
 
   pinStart.removeEventListener('mouseup', activeSitePageHandle);
 }
@@ -45,3 +51,12 @@ function activeSitePageHandle() {
 
 pinStart.addEventListener('mouseup', activeSitePageHandle);
 
+
+form.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  window.backend.save(new FormData(form), function () {
+    //console.log(new FormData(form));
+    userDialog.classList.add('hidden');
+  }, window.backend.errorHandler);
+  form.reset();
+});
